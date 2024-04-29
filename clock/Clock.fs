0ab47b2721f3376e7366c9e20915d9a09c77038d
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-04-29 13:02:16 +0300

    Add clock module with tests and project setup
    Implemented a Clock module, which can handle adding and subtracting minutes to a time without dates and compares two equivalent times effectively. Alongside the module, test cases were created to ensure the functionality works. The project's F# setup was also crafted and dependencies added to support it.

---
+++ clock/Clock.fs
@@ -0,0 +1,31 @@
+module Clock
+
+[<Literal>]
+let private minutesPerHour = 60
+[<Literal>]
+let private hoursPerDay = 24
+
+[<Literal>]
+let private minutesPerDay = minutesPerHour * hoursPerDay
+
+type Clock =
+    { Hours: int
+      Minutes: int }
+
+let create hours minutes =
+    let mutable h = hours + minutes / minutesPerHour
+    let mutable m = minutes % minutesPerHour
+    if m < 0 then
+        h <- h - 1
+        m <- m + minutesPerHour
+    h <- h % hoursPerDay
+    if h < 0 then h <- h + hoursPerDay
+
+    { Hours = h
+      Minutes = m }
+
+let add minutes clock = create clock.Hours (clock.Minutes + minutes)
+
+let subtract minutes clock = create clock.Hours (clock.Minutes - minutes)
+
+let display clock = $"%02d{clock.Hours}:%02d{clock.Minutes}"
