c9ddb232676ec502a89364d5b902f3a780338858
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-04 11:18:55 +0300

    Add BirdWatcher exercise with related unit tests
    This commit introduces the BirdWatcher exercise. It provides functionality to track and manipulate data related to bird sightings over a week. Additionally, fixtures for unit testing have been added to validate the behavior of the new functionality under various scenarios. A README file was also added to provide information and instructions. Minor changes were made to Grains.fs as well.
    
    @coderabbitai ignore

--- grains/Grains.fs
+++ grains/Grains.fs
@@ -1,7 +1,8 @@
 module Grains
 
-let square (n: int): Result<uint64,string> =
-    if n >= 1 && n <= 64 then 1UL <<< n - 1 |> Ok
+let square (n: int): Result<uint64, string> =
+    if n >= 1 && n <= 64
+    then 1UL <<< n - 1 |> Ok
     else Error "square must be between 1 and 64"
-    
-let total: Result<uint64,string> = Ok ~~~0UL
+
+let total: Result<uint64, string> = Ok ~~~0UL
