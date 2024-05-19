480598022835381106736bcc47ee3baf62465aa7
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-19 11:08:59 +0300

    Refactor String.concat in Raindrops & optimize KindergartenGarden
    Changed the string concatenation logic in Raindrops.fs to use System.String.Concat. Also optimized the KindergartenGarden.fs by replacing map function with collect for string concatenation, and streamlined 'toPlant' function.

--- kindergarten-garden/KindergartenGarden.fs
+++ kindergarten-garden/KindergartenGarden.fs
@@ -6,20 +6,19 @@ type Plant =
     | Radishes
     | Violets
 
-let toPlant symbol =
-    match symbol with
+let private toPlant =
+    function
     | 'G' -> Grass
     | 'C' -> Clover
     | 'R' -> Radishes
     | 'V' -> Violets
-    | _ -> failwith $"Unknown symbol '{symbol}'"
+    | symbol -> failwith $"Unknown symbol '{symbol}'"
     
 let plants (diagram: string) (student:string) =
-    let startIndex = 2 * int (student[ 0 ] - 'A')
+    let startIndex = 2 * int (student[0] - 'A')
     
     diagram.Split('\n')
-    |> Seq.map _.Substring(startIndex, 2)
-    |> String.concat ""
+    |> Seq.collect _.Substring(startIndex, 2)
     |> Seq.map toPlant
     |> Seq.toList
 
